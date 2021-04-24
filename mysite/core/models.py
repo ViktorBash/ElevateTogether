641a0cb6ea5af823f186b725ccb3540fa22a7c25
ViktorBash <basharkevichv@gmail.com>
2021-04-24 14:49:27 -0400

    Deleted .idea, changed requirements, created profie model

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    knows_math = models.BooleanField(default=False, null=False)
+    knows_singing = models.BooleanField(default=False, null=False)
+    knows_music = models.BooleanField(default=False, null=False)
+    knows_drawing = models.BooleanField(default=False, null=False)
+    knows_video_editing = models.BooleanField(default=False, null=False)
+    knows_animation = models.BooleanField(default=False, null=False)
