d22094cb598fde1ceda423d981336b6312c9eb9f
ViktorBash <basharkevichv@gmail.com>
2021-04-25 10:12:59 -0400

    Fixed image issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
 )
+# STATIC_ROOT = 
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
