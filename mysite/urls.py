1d2a6a623803144f3eb367c55918ceb8d91803af
ViktorBash <basharkevichv@gmail.com>
2021-04-25 10:30:17 -0400

    Changed login bug

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('', views.landing, name='home'),
     path('learn/', views.learn, name='learn'),
     path('teach/', views.teach, name='teach'),
     path('signup/', views.signup, name='signup'),
